6dba102b50168764fb33f25a4c8741cd671112c6
Aditya Gupta <aditya3492gupta@gmail.com>
2025-05-06 15:52:29 +0530

    changes in all

--- entity/SilverEventSpace.java
+++ entity/SilverEventSpace.java
@@ -3,13 +3,12 @@ package entity;
 public class SilverEventSpace extends EventSpace {
     private double bookingCost;
     private boolean hasCatering;
-    
 
     public SilverEventSpace(String spaceId, boolean isAvailable) {
         super(spaceId, "Silver", isAvailable);
-        this.bookingCost = 2000.0; 
-        this.hasCatering = false;   
-        
+        this.bookingCost = 2000.0;
+        this.hasCatering = false;
+
     }
 
     // Getters and setters for new properties
@@ -28,7 +27,5 @@ public class SilverEventSpace extends EventSpace {
     public void setHasCatering(boolean hasCatering) {
         this.hasCatering = hasCatering;
     }
-
-    
 
 }
--- service/EventSpaceService.java
+++ service/EventSpaceService.java
@@ -1,6 +1,6 @@
 package service;
 
-import entity.EventSpace;
+import entity.*;
 import repository.EventSpaceRepository;
 
 import java.util.List;
@@ -52,5 +52,37 @@ public class EventSpaceService {
     // Find available EventSpaces by type
     public List<EventSpace> searchAvailableSpacesByType(String type) {
         return eventSpaceRepository.findAvailableSpacesByType(type);
+    }
+
+    // Book an available EventSpace of a given type and show cost
+    public void bookEventSpaceByType(String type) {
+        List<EventSpace> available = eventSpaceRepository.findAvailableSpacesByType(type);
+
+        if (available.isEmpty()) {
+            System.out.println("No available " + type + " event spaces.");
+            return;
+        }
+
+        EventSpace selected = available.get(0);
+        selected.setAvailable(false);
+        eventSpaceRepository.updateEventSpace(selected);
+
+        double cost = 0.0;
+        boolean hasCatering = false;
+
+        if (selected instanceof GoldEventSpace gold) {
+            cost = gold.getBookingCost();
+            hasCatering = gold.hasCatering();
+        } else if (selected instanceof SilverEventSpace silver) {
+            cost = silver.getBookingCost();
+            hasCatering = silver.hasCatering();
+        } else if (selected instanceof PlatinumEventSpace platinum) {
+            cost = platinum.getBookingCost();
+            hasCatering = platinum.hasCatering();
+        }
+
+        System.out.println("Event space booked: " + selected.getSpaceId());
+        System.out.println("Cost: ₹" + cost);
+        System.out.println("Catering included: " + (hasCatering ? "Yes" : "No"));
     }
 }
