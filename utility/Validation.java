e33fa8f2b20537eaa7ba142ec6588b7f16859ecc
6SHREYA <shreya06lg@gmail.com>
2025-05-07 11:11:23 +0530

    utility validate phn no

--- utility/Validation.java
+++ utility/Validation.java
@@ -3,8 +3,9 @@ package utility;
 import java.util.regex.Pattern;
 //validation
 public class Validation {
-     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$");
     private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$");
+    private static final Pattern PHONE_PATTERN = Pattern.compile("^[0-9]{10}$");
 
     public static boolean isValidEmail(String email) {
         return EMAIL_PATTERN.matcher(email).matches();
@@ -12,6 +13,10 @@ public class Validation {
 
     public static boolean isValidPassword(String password) {
         return PASSWORD_PATTERN.matcher(password).matches();
+    }
+
+    public static boolean isValidPhoneNumber(String phone) {
+        return PHONE_PATTERN.matcher(phone).matches();
     }
     
 }
