5632b7e85f79639668254847c680234643c469c2
Aditya Gupta <aditya3492gupta@gmail.com>
2025-05-08 11:31:02 +0530

    getter n setter

--- entity/cart/EventSpaceCartItem.java
+++ entity/cart/EventSpaceCartItem.java
@@ -3,19 +3,27 @@ package entity.cart;
 public class EventSpaceCartItem extends CartItem {
     private String eventType;
     private int noOfDays;
-    
-    public EventSpaceCartItem(String resourceId, String eventType, double cost,int noOfDays) {
-        super(resourceId, "EventSpace", cost*noOfDays);
+
+    public EventSpaceCartItem(String resourceId, String eventType, double cost, int noOfDays) {
+        super(resourceId, "EventSpace", cost * noOfDays);
         this.eventType = eventType;
-        this.noOfDays=noOfDays;
+        this.noOfDays = noOfDays;
     }
-    
+
     public String getEventType() {
         return eventType;
     }
-    
+
     @Override
     public String getDescription() {
         return "Event Space " + getResourceId() + " (" + eventType + ")";
+    }
+
+    public int getNoOfDays() {
+        return this.noOfDays;
+    }
+
+    public void setNoOfDays(int noOfDays) {
+        this.noOfDays = noOfDays;
     }
 }
--- entity/cart/TransportationCartItem.java
+++ entity/cart/TransportationCartItem.java
@@ -3,19 +3,27 @@ package entity.cart;
 public class TransportationCartItem extends CartItem {
     private String vehicleType;
     private int noOfDays;
-    
+
     public TransportationCartItem(String resourceId, String vehicleType, double cost, int noOfDays) {
-        super(resourceId, "Transportation", cost*noOfDays);
+        super(resourceId, "Transportation", cost * noOfDays);
         this.vehicleType = vehicleType;
-        this.noOfDays=noOfDays;
+        this.noOfDays = noOfDays;
     }
-    
+
     public String getVehicleType() {
         return vehicleType;
     }
-    
+
     @Override
     public String getDescription() {
         return "Vehicle " + getResourceId() + " (" + vehicleType + ")";
+    }
+
+    public int getNoOfDays() {
+        return this.noOfDays;
+    }
+
+    public void setNoOfDays(int noOfDays) {
+        this.noOfDays = noOfDays;
     }
 }
